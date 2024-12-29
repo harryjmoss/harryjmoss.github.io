45bc96cce46959640c708a7771d2b84cb0072c0d
Harry Moss <50363528+harryjmoss@users.noreply.github.com>
2024-12-29 16:54:23 +0000

    Add GitHub workflow checks (#69)
    * Update NPM build job to also lint with prettier.
    
    * Format with prettier
    
    * Format .github yaml
    
    * Lint dependabot automerge yaml

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Personal Website
 
-This is the repo for my personal website, built using [Astro](https://astro.build/) with the [Brutal](https://github.com/eliancodes/brutal) theme. 
+This is the repo for my personal website, built using [Astro](https://astro.build/) with the [Brutal](https://github.com/eliancodes/brutal) theme.
 
 The theme mentions:
+
 > The theme has no JavaScript integration out of the box, but can always be added of course.
 
 which is exactly what I was looking for here!
 
 There is an RSS feed that outputs blog post content. I'll also be adding a Talks page, where the talks will be written in `reveal.js`.
 
-
 ## Local Dev Usage
 
 I'm using `npm` here - `pnpm` is also a valid choice.
 
 Start the site with
+
 ```bash
 npm run dev
 ```
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/blog" }),
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/data/blog' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -13,19 +13,23 @@ const blogCollection = defineCollection({
       pubDate: z.string().transform((str) => {
         // Split the date and time parts
         const [datePart, timePart] = str.split(' ');
-        
+
         // Split the date into day, month, and year
         const [day, month, year] = datePart.split('/').map(Number);
-        
+
         // Default time to midnight if not provided
-        const [hours, minutes] = timePart ? timePart.split(':').map(Number) : [0, 0];
-        
+        const [hours, minutes] = timePart
+          ? timePart.split(':').map(Number)
+          : [0, 0];
+
         // Create a Date object
         const parsedDate = new Date(year, month - 1, day, hours, minutes);
 
         // Check if the parsedDate is invalid
         if (isNaN(parsedDate.getTime())) {
-          throw new Error(`Invalid date format: ${str}. Expected format is dd/mm/yyyy HH:mm.`);
+          throw new Error(
+            `Invalid date format: ${str}. Expected format is dd/mm/yyyy HH:mm.`
+          );
         }
 
         return parsedDate;
@@ -38,4 +42,3 @@ const blogCollection = defineCollection({
 export const collections = {
   blog: blogCollection,
 };
-
